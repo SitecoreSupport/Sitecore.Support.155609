533643feec0e7abade8ed58b9cebad9aadc1bae0
DK\ili <ili@sitecore.net>
2017-04-04 14:12:56 +0300

    Release

---
+++ src/Sitecore.Support.155609/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.155609")]
+[assembly: AssemblyProduct("Sitecore.Support.155609")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("patch file MessageContext.js")]
+
